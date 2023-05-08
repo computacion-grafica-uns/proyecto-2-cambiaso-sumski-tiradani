b0347060c80e2f165fa098bbff603d65ead13ee0
Emanuel Tiradani <ematiradani@gmail.com>
2023-05-08 18:19:08 -0300

    1, 2, 3 para el toggle de las luces
    1-DirectionalLight 2-PointLight 3-SpotLight

--- Assets/Scripts/ManejadorLuces.cs
+++ Assets/Scripts/ManejadorLuces.cs
@@ -1,5 +1,7 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class ManejadorLuces : MonoBehaviour
@@ -8,16 +10,22 @@ public class ManejadorLuces : MonoBehaviour
     public GameObject pointLight;
     public GameObject spotLight;
 
+    public Material[] materiales;
+
 
     public Color direcColor = new Color(0, 0, 1);
     public Color pointColor = new Color(0, 1, 0);
     public Color spotColor = new Color(1, 0, 0);
 
+    Color oldPointLightColor;
+    Color oldSpotLightColor;
+    Vector4 oldDirectionalLightDirection;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        materiales = Resources.LoadAll("Materiales", typeof(Material)).Cast<Material>().ToArray();
     }
 
     // Update is called once per frame
@@ -28,12 +36,56 @@ public class ManejadorLuces : MonoBehaviour
         // Agarrar la pos de los 3 gameobjects y los colores
         // y pasarselo a todos los materiales que existan
         // para que se actualice con la posicion de la luz
+        
+        foreach (Material material in materiales)
+        {
+            oldSpotLightColor = material.GetColor("_SpotLightColor");
+            oldPointLightColor = material.GetColor("_PointLightColor");
+            oldDirectionalLightDirection = material.GetVector("_DirectionalLightDirection_w");
+        }
 
-        // Hacer lo del input para con 3 teclas desactivar o activar cada luz
-
-            // La spot y point con ponerlas en negro se desactivan
-
-            // La direccional hay que si o si ponerle todo el vector direccion en 0,0,0
-            // preguntarle a mati 
+        // Directional light
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            foreach (Material material in materiales)
+            {
+                if(!material.GetVector("_DirectionalLightDirection_w").Equals(new Vector4(0, 0, 0, 1))){
+                    oldDirectionalLightDirection = material.GetVector("_DirectionalLightDirection_w");
+                    material.SetVector("_DirectionalLightDirection_w", new Vector4(0, 0, 0, 1));
+                }
+                else
+                {
+                    material.SetVector("_DirectionalLightDirection_w", oldDirectionalLightDirection);
+                }
+            }
+        }
+        // Point light
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            foreach (Material material in materiales)
+            {
+                if (material.GetColor("_PointLightColor") != Color.black)
+                {
+                    oldPointLightColor = material.GetColor("_PointLightColor");
+                    material.SetColor("_PointLightColor", Color.black);
+                }
+                else
+                    material.SetColor("_PointLightColor", oldPointLightColor);
+            }
+        }
+        // SpotLight
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            foreach (Material material in materiales)
+            {
+                if (material.GetColor("_SpotLightColor") != Color.black)
+                {
+                    oldSpotLightColor = material.GetColor("_SpotLightColor");
+                    material.SetColor("_SpotLightColor", Color.black);
+                }
+                else
+                    material.SetColor("_SpotLightColor", oldSpotLightColor);
+            }
+        }
     }
 }
