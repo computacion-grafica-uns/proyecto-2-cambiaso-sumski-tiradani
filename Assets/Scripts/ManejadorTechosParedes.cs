9a7ce00dd5d3573b15e1c484950cd31518157667
Emanuel Tiradani <ematiradani@gmail.com>
2023-05-15 19:35:12 -0300

    Activar/Desactivar paredes y techo

--- Assets/Scripts/ManejadorTechosParedes.cs
+++ Assets/Scripts/ManejadorTechosParedes.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,15 +9,31 @@ public class ManejadorTechosParedes : MonoBehaviour
     public GameObject carpetaTechos;
     public GameObject carpetaParedes;
 
+    public Boolean techoActivo;
+    public Boolean paredesActivas;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        techoActivo = true;
+        paredesActivas = true;
+        carpetaTechos = GameObject.Find("Techo");
+        carpetaParedes = GameObject.Find("Paredes");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            techoActivo = !techoActivo;
+            carpetaTechos.SetActive(techoActivo);
+            
+        }
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            paredesActivas = !paredesActivas;
+            carpetaParedes.SetActive(paredesActivas);
+        }
     }
 }
